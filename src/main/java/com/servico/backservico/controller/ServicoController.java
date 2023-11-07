2dc756f88129f4282578af836e32f788dc710a08
Palc3301 <pedroarruda8@live.com>
2023-11-07 17:55:35 -0300

    Change SQL query in repository & changes in service/controller

--- src/main/java/com/servico/backservico/controller/ServicoController.java
+++ src/main/java/com/servico/backservico/controller/ServicoController.java
@@ -1,9 +1,9 @@
 package com.servico.backservico.controller;
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,37 +24,44 @@ public class ServicoController {
 	private ServicoService servicoService;
 	
 	@GetMapping("/")
+	@CrossOrigin("http://localhost:3000")
 	public List<Servico> buscarTodos(){
 		return servicoService.buscarTodos();
 	}
 	
 	@GetMapping("/pagamentoPendente")
+	@CrossOrigin("http://localhost:3000")
 	public List<Servico> buscarServicosPagamanetosPendente(){
 		return servicoService.buscarServicosPagamanetosPendente();
 	}
 	
 	@GetMapping("/cancelados")
+	@CrossOrigin("http://localhost:3000")
 	public List<Servico> buscarServicosCancelados() {
 		return servicoService.buscarServicosCancelados();
 	}
 	
 	@PostMapping("/")
+	@CrossOrigin("http://localhost:3000")
 	public Servico inserir(@RequestBody Servico servico) {
 		return servicoService.inserir(servico);
 	}
 	
 	@PostMapping("/{id}")
+	@CrossOrigin("http://localhost:3000")
 	public ResponseEntity<Void> cancelar(@PathVariable("id") Long id) {
 		servicoService.cancelarServico(id);
 		return ResponseEntity.ok().build();
 	}
 	
 	@PutMapping("/")
+	@CrossOrigin("http://localhost:3000")
 	public Servico alterar(@RequestBody Servico servico) {
 		return servicoService.alterar(servico);
 	}
 	
 	@DeleteMapping("/{id}")
+	@CrossOrigin("http://localhost:3000")
 	public ResponseEntity<Void> remover(@PathVariable("id") Long id){
 		servicoService.remover(id);
 		return ResponseEntity.ok().build();
